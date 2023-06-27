af4d26f21d439d28bd76e265496e49e2fc0e1385
Darren <hldaz77@gmail.com>
2023-06-27 17:12:40 +0100

    added three charts

--- app.js
+++ app.js
@@ -8,18 +8,136 @@ const maxClicksAllowed = 25;
 
 let allProducts = [];
 
+// Collect data for the chart
+function collectChartData() {
+  const dataClicks = [];
+  const dataViews = [];
+  const labels = [];
+
+  for (const product of allProducts) {
+    dataClicks.push(product.clicks);
+    dataViews.push(product.views);
+    labels.push(product.name);
+  }
+
+  //console.log("Data Clicks:", dataClicks);
+  //console.log("Data Views:", dataViews);
+  //console.log("Labels:", labels);
+
+  const fixedColors = ["#6495ED", "#40E0D0"];
+  const clicksDataset = {
+    label: "Clicks",
+    data: dataClicks,
+    backgroundColor: fixedColors[0],
+    borderColor: fixedColors[0],
+    borderWidth: 1,
+  };
+
+  const viewsDataset = {
+    label: "Views",
+    data: dataViews,
+    backgroundColor: fixedColors[1],
+    borderColor: fixedColors[1],
+    borderWidth: 1,
+  };
+
+  return {
+    clicksDataset,
+    viewsDataset,
+    labels,
+  };
+}
+//Render chart
+function renderChart(chartType, datasets, labels) {
+  const ctx = document.getElementById("myChart").getContext("2d");
+
+  const config = {
+    type: chartType,
+    data: {
+      labels: labels,
+      datasets: datasets.map((dataset) => {
+        if (chartType === "bar") {
+          return {
+            ...dataset,
+            backgroundColor: dataset.backgroundColor,
+            borderColor: dataset.borderColor,
+          };
+        } else {
+          return {
+            ...dataset,
+            backgroundColor: generateRandomColors(labels.length),
+            borderColor: "#FFFF00",
+          };
+        }
+      }),
+    },
+    options: {
+      responsive: true,
+      maintainAspectRatio: false,
+      aspectRatio: 1,
+      scales: {
+        y: {
+          beginAtZero: true,
+        },
+      },
+    },
+  };
+
+  if (myChart) {
+    myChart.destroy();
+  }
+
+  myChart = new Chart(ctx, config);
+}
+//Random colour for pie and doughnut charts
+function generateRandomColors(count) {
+  const colors = [];
+  for (let i = 0; i < count; i++) {
+    const color = "#" + Math.floor(Math.random() * 16777215).toString(16);
+    colors.push(color);
+  }
+  return colors;
+}
+//results button
+resultsButton.addEventListener("click", () => {
+  if (!myChart) {
+    const chartData = collectChartData();
+    renderChart(
+      "bar",
+      [chartData.clicksDataset, chartData.viewsDataset],
+      chartData.labels
+    );
+    resultsButton.textContent = "See More Charts";
+  } else if (myChart.config.type === "bar") {
+    const chartData = collectChartData();
+    renderChart("pie", [chartData.clicksDataset], chartData.labels);
+    resultsButton.textContent = "See More Charts";
+  } else if (myChart.config.type === "pie") {
+    const chartData = collectChartData();
+    renderChart("doughnut", [chartData.viewsDataset], chartData.labels);
+    resultsButton.style.display = "none";
+  }
+  image1.style.display = "none";
+  image2.style.display = "none";
+  image3.style.display = "none";
+});
+
+// Get a random product number
 function getRandomNumber() {
   return Math.floor(Math.random() * allProducts.length);
 }
 
+// Product constructor
 function Product(name, src) {
   this.name = name;
   this.src = src;
   this.clicks = 0;
   this.views = 0;
   allProducts.push(this);
+  //console.log(allProducts);
 }
 
+// Render product images
 function renderProducts() {
   let product1 = getRandomNumber();
   let product2 = getRandomNumber();
@@ -45,6 +163,7 @@ function renderProducts() {
   allProducts[product3].views++;
 }
 
+// Handle product click
 function handleProductClick(event) {
   if (event.target === productContainer) {
     alert("Please click on an image");
@@ -61,8 +180,7 @@ function handleProductClick(event) {
     if (clicks === maxClicksAllowed) {
       productContainer.removeEventListener("click", handleProductClick);
       productContainer.className = "no-voting";
-      resultsButton.addEventListener("click", renderResults);
-      resultsButton.textContent = "View Results"; // Change button text to "View Results"
+      resultsButton.textContent = "View Results";
       resultsButton.className = "clicks-allowed";
     } else {
       renderProducts();
@@ -70,15 +188,7 @@ function handleProductClick(event) {
   }
 }
 
-function renderResults() {
-  let ul = document.querySelector("ul");
-  for (let i = 0; i < allProducts.length; i++) {
-    let li = document.createElement("li");
-    li.textContent = `${allProducts[i].name} had ${allProducts[i].views} views and was clicked ${allProducts[i].clicks} times.`;
-    ul.appendChild(li);
-  }
-}
-
+// Create products
 const bag = new Product("Bag", "assets/images/bag.jpg");
 const banana = new Product("Banana", "assets/images/banana.jpg");
 const bathroom = new Product("Bathroom", "assets/images/bathroom.jpg");
@@ -99,8 +209,9 @@ const unicorn = new Product("Unicorn", "assets/images/unicorn.jpg");
 const watercan = new Product("Water Can", "assets/images/water-can.jpg");
 const wineglass = new Product("Wine Glass", "assets/images/wine-glass.jpg");
 
+// Initial rendering
 renderProducts();
-
 resultsButton.textContent = "Vote Now";
-
 productContainer.addEventListener("click", handleProductClick);
+
+let myChart = null;
